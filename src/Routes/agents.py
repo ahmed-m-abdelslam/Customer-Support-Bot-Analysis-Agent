2f412a32056f68e011d63ec6e611c249b04f1473
ahmed-m-abdelslam <amjj6367@gmail.com>
2026-01-28 05:19:18 +0200

    Refactor agent initialization in process_conversation for improved readability

--- src/Routes/agents.py
+++ src/Routes/agents.py
@@ -18,20 +18,13 @@ async def process_conversation( request: Request, payload: dict ):
     data = load_data()
 
     conversation= data[payload["data_row"]]["conversation"]
-        
-    Conversation_Parser = ConversationParserAgent()
-    Context_Builder = ContextBuilderAgent()
-    Intent_Detection = IntentDetectionAgent()
-    Sentiment_Analyzer = SentimentAgent()
-    Decision_Maker = DecisionAgent()
-    Response_Generator = ResponseAgent()
 
-    agent1, task1 = Conversation_Parser.run()
-    agent2, task2 = Context_Builder.run()
-    agent3, task3 = Intent_Detection.run(task_name=task2)
-    agent4, task4 = Sentiment_Analyzer.run(task_name=task2)
-    agent5, task5 = Decision_Maker.run(task1_name=task3, task2_name=task4)
-    agent6, task6 = Response_Generator.run()
+    agent1, task1 = ConversationParserAgent().run()
+    agent2, task2 = ContextBuilderAgent().run()
+    agent3, task3 = IntentDetectionAgent().run(task_name=task2)
+    agent4, task4 = SentimentAgent().run(task_name=task2)
+    agent5, task5 = DecisionAgent().run(task1_name=task3, task2_name=task4)
+    agent6, task6 = ResponseAgent().run()
 
     crew = Crew(
                 agents=[agent1,
